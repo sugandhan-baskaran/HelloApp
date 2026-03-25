541536a5825ce13e3f126061742c973b671f0806
sugandhan-baskaran <sugandhanbaskaran@gmail.com>
2026-03-25 09:48:28 +0530

    Added all UC files to dev properly

---
+++ O6UC5.java
@@ -0,0 +1,18 @@
+public class O6UC5 {
+
+public static void main(String[] args) {
+if (args.length == 0) {
+System.out.println("Hello, World!");
+} else {
+StringBuilder names = new StringBuilder();
+for (String name : args) {
+if (names.length() > 0) {
+names.append(", ");
+}
+
+names.append(name);
+}
+System.out.println("Hello, " + names + "!");
+}
+}
+}
